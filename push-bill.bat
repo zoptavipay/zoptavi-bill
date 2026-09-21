a6aad26794bb2b27ff5a0d45a453ce747a9a88d3
Zoptavi <hussain@zoptavi.com>
2026-09-21 18:07:42 +0530

    Update push script commit message

--- push-bill.bat
+++ push-bill.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 git add .
-git commit -m "Update Zoptavi Bill %date% %time%"
+git commit -m "Update Zoptavi Tab %date% %time%"
 git push origin main
 echo ========================================
 echo   Done. Press any key to close.
